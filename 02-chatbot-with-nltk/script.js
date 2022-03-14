4a9c0f5422e02daf4be0eefe062ad5b3e18d8f93
Eileen <xue.eileen@gmail.com>
2022-03-13 22:54:13 -0400

    Upload basic chatbot from tutorial

---
+++ 02-chatbot-with-nltk/script.js
@@ -0,0 +1,54 @@
+// Events
+$(document).on('click', '#speak', recognize);
+$(document).on('submit', '#user', send);
+
+// Variables
+let recognition = new webkitSpeechRecognition();
+
+recognition.onresult = function (e) {
+  // console.log(e.results);
+
+  let transcript = e.results[0][0].transcript;
+
+  $('#msg').val(transcript);
+  recognition.stop();
+  $('#speak').removeClass('activated');
+  $('#user').submit();
+};
+
+// Functions
+function recognize() {
+  // alert('!');
+  recognition.start();
+  $(this).addClass('activated');
+}
+
+function send(e) {
+  e.preventDefault();
+
+  let msg = $('#msg').val();
+
+  if (msg === '') {
+    return false;
+  }
+  $('#chat').append('YOU: ');
+  $('#chat').append(msg);
+  $('#chat').append('\n');
+
+  $('#msg').val('');
+
+  let path = 'eileen.xue/eliza/chat.py';
+  let url = 'https://ocadu.goodcodeclub.com/chat?path=' + path + '&msg=' + msg;
+
+  $.ajax({
+    url: url,
+    success: insert,
+  });
+}
+
+function insert(output) {
+  $('#chat').append('BOT: ');
+  $('#chat').append(output);
+  $('#chat').append('\n');
+  $('#chat').scrollTop(99999999);
+}
